95f40964c567db033e4abbc4856fc114113a138c
rosmat <rosmat@o2.pl>
2016-01-26 13:32:39 +0100

    Adding persons

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
-import play.*;
+import com.avaje.ebean.Model;
+import models.Person;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
+import static play.libs.Json.toJson;
+
 public class Application extends Controller {
 
+
+    public Result addPerson() {
+        Person person = Form.form(Person.class).bindFromRequest().get();
+        person.save();
+        return redirect(routes.Application.index());
+    }
+
+    public Result getPersons() {
+        List<Person> persons = new Model.Finder(String.class, Person.class).all();
+        return ok(toJson(persons));
+    }
+
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
 
 }
